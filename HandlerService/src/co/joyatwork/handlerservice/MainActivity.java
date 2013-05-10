4700054e92afa644326fc566da3de0166a0db580
ihla <lubos.ilcik@gmail.com>
2013-05-10 14:11:01 +0200

    - service running SensorEventListener in helper thread

--- HandlerService/src/co/joyatwork/handlerservice/MainActivity.java
+++ HandlerService/src/co/joyatwork/handlerservice/MainActivity.java
@@ -2,7 +2,10 @@ package co.joyatwork.handlerservice;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,22 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		
+		Button quitButton = (Button) findViewById(R.id.quitButton);
+		quitButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// stop service implicitly
+				Intent intent = new Intent(MainActivity.this, HelperService.class);
+				stopService(intent);
+				finish();
+			}
+		});
+		
+		// start service explicitly
+		Intent intent = new Intent(MainActivity.this, HelperService.class);
+		startService(intent);
 	}
 
 	@Override
